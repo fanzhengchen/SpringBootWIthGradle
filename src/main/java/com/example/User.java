feab09ba129d0f97160d8d32018d8863936827d6
fanzhengchen <fanzhengchen@gmail.com>
2016-02-04 19:03:39 +0800

    init

--- src/main/java/com/example/User.java
+++ src/main/java/com/example/User.java
@@ -1,7 +1,36 @@
 package com.example;
 
+import javax.print.DocFlavor;
+
 /**
  * Created by mark on 16-2-4.
  */
 public class User {
+    private String name;
+    private String gender;
+
+    public User() {
+
+    }
+
+    public User(String name, String gender) {
+        this.name = name;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
 }
